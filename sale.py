70ec500362e0d74e2ca97d1fa69c69bc809bfdd5
lukhen <lukasz.hen@gmail.com>
2018-10-26 05:24:57 +0200

    Now PointOfSail behaves properly if the inventory is empty.

--- sale.py
+++ sale.py
@@ -2,17 +2,36 @@ from enum import Enum
 
 
 class Display:
+    def __init__(self):
+        self._text = ''
+
     def text(self):
-        return DisplayMessages.INVALID_BARCODE.value
+        return self._text
+
+    def print_message(self, message):
+        self._text = message
+
+
+class Inventory:
+    def __len__(self):
+        return 0
 
 
 class PointOfSale:
-    def __init__(self, display):
-        pass
+    def __init__(self, display, inventory=Inventory()):
+        self._display = display
+        self._inventory = inventory
 
     def onbarcode(self, barcode):
-        pass
+        if barcode == '':
+            self._display.print_message(
+                DisplayMessages.INVALID_BARCODE.value)
+
+        elif len(self._inventory) == 0:
+            self._display.print_message(
+                DisplayMessages.PRODUCT_NOT_FOUND.value)
 
 
 class DisplayMessages(Enum):
     INVALID_BARCODE = 'Invalid barcode'
+    PRODUCT_NOT_FOUND = 'Product not found'
--- tests/test_display_price.py
+++ tests/test_display_price.py
@@ -6,3 +6,10 @@ def test_barcode_is_empty_string():
     pos = PointOfSale(display)
     pos.onbarcode('')
     assert display.text() == DisplayMessages.INVALID_BARCODE.value
+
+
+def test_Inventory_is_empty():
+    display = Display()
+    pos = PointOfSale(display)
+    pos.onbarcode('123')
+    assert display.text() == DisplayMessages.PRODUCT_NOT_FOUND.value
