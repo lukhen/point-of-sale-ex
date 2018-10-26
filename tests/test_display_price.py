62174817578a4399e6b0df78be48c9b76cb2080d
lukhen <lukasz.hen@gmail.com>
2018-10-26 06:54:10 +0200

    Added test to check invalid barcode, but skipped it until Barcode class is introduced.

--- tests/test_display_price.py
+++ tests/test_display_price.py
@@ -1,4 +1,5 @@
 from sale import Display, PointOfSale, DisplayMessages
+import pytest
 
 
 def test_barcode_is_empty_string():
@@ -13,3 +14,11 @@ def test_Inventory_is_empty():
     pos = PointOfSale(display)
     pos.onbarcode('123')
     assert display.text() == DisplayMessages.PRODUCT_NOT_FOUND.value
+
+
+@pytest.mark.skip(reason="wait until Barcode class is implemented and tested")
+def test_barcode_contining_not_only_digits():
+    display = Display()
+    pos = PointOfSale(display)
+    pos.onbarcode('a23')
+    assert display.text() == DisplayMessages.INVALID_BARCODE.value
