57d5976e67540e9d20eecf57ae210b055b5ba40e
lukhen <lukasz.hen@gmail.com>
2018-10-28 07:12:23 +0100

    Added additional example of comparing money.

--- tests/test_adding_money.py
+++ tests/test_adding_money.py
@@ -4,3 +4,7 @@ from sale import Money, Currency
 def test_equalisty_money_with_the_same_currency():
     assert Money(1, Currency.USD) == Money(1, Currency.USD)
     assert Money(1, Currency.USD) != Money(2, Currency.USD)
+
+
+def test_equality_with_different_currency():
+    assert Money(1, Currency.PLN) != Money(1, Currency.USD)
