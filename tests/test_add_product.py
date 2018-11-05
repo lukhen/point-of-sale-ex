c740132da9a20ad9c4a7ea9430fa9b536e1b28ed
lukhen <lukasz.hen@gmail.com>
2018-11-05 07:00:14 +0100

    Removed obsolete code.

--- tests/test_add_product.py
+++ tests/test_add_product.py
@@ -1,11 +1,11 @@
-from sale import Inventory, Barcode, Money, Product
+from sale import Inventory, Barcode, Money, Product, Currency
 import pytest
 
 
 def test_add_product_to_empty_inventory():
     i = Inventory()
     barcode = Barcode('123')
-    price = Money(5.4, '$')
+    price = Money(5.4, Currency.USD)
     p = Product(barcode, 'Book', price)
     assert p not in i
     i.add_product(p)
