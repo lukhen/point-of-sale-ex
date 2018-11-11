743c02770396a5800705bff16b6383ad0a0215c8
lukhen <lukasz.hen@gmail.com>
2018-11-11 06:45:43 +0100

    Additional test of retrieving not existing product form unempty inventory.

--- tests/test_get_product.py
+++ tests/test_get_product.py
@@ -15,3 +15,10 @@ def test_get_product_when_it_exists():
     assert retrieved is not None
     assert retrieved.name == p.name
     assert retrieved.price == p.price
+
+
+def test_get_not_existing_product_from_unempty_inventory():
+    i = Inventory()
+    p = Product(Barcode('123'), 'book', Money(10, Currency.USD))
+    i.add_product(p)
+    assert i.get_product_by_barcode(Barcode('234562')) is None
